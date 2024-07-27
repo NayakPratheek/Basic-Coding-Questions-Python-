c6d3d9a4aee019e5a93b9a50be48a87672982aee
P Nidhi Padiyar <120041618+Nidhi-Padiyar-01@users.noreply.github.com>
2024-07-28 01:10:24 +0530

    Update 3.prime_number.py

--- Most asked basic Questions/3.prime_number.py
+++ Most asked basic Questions/3.prime_number.py
@@ -5,32 +5,14 @@ def prime(n):
         return False
     if n==2:
         return True
-    for i in range(2,n**0.5+1):
+    for i in range(2,int(n**0.5)+1):
         if n%i==0:
             return False
     return True
 n = int(input("enter the number:"))
 prime(n)
-
-or
-
-def prime(n):
-    if n <= 1:
-        return False  # 0, 1, and negative numbers are not prime
-    if n == 2:
-        return True  # 2 is the only even prime number
-    if n % 2 == 0:
-        return False  # other even numbers are not prime
-
-    # Check only odd numbers from 3 up to sqrt(n)
-    for i in range(3, int(n ** 0.5) + 1, 2):#here 2 refers to the step size,checking only odd numbers
-        if n % i == 0:
-            return False
-    return True
-
-
-n = int(input("Enter the number: "))
 if prime(n):
     print(f"{n} is a prime number.")
 else:
     print(f"{n} is not a prime number.")
+
